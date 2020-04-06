7838e9c5d05234a04296fe494b52d70911d8e030
Carlos Diaz <carlosandresdiazbravo@Carloss-MacBook-Pro.local>
2020-04-06 16:22:56 -0700

    lab8.1

---
+++ Lab8/Kitchen.h
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Lab8
+//
+//  Created by Carlos andres Diaz bravo  on 2020-04-06.
+//  Copyright © 2020 Carlos andres Diaz bravo . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Kitchen : NSObject
+
+- (Pizza *)makePizzaWithSize:(PizzaSize)size toppings:(NSArray *)toppings;
+
+@end
---
+++ Lab8/Kitchen.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lab8
+//
+//  Created by Carlos andres Diaz bravo  on 2020-04-06.
+//  Copyright © 2020 Carlos andres Diaz bravo . All rights reserved.
+//
+#import "Kitchen.h"
+
+@implementation Kitchen
+
+- (Pizza *)makePizzaWithSize:(PizzaSize)size toppings:(NSArray *)toppings
+{
+    return toppings;
+}
+
+@end
