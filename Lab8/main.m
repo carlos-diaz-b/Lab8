0a48b8a9189c9ecb49be7f64e63306e7520f1e4f
Carlos Diaz <carlosandresdiazbravo@Carloss-MacBook-Pro.local>
2020-04-06 16:00:22 -0700

    lab8

---
+++ Lab8/main.m
@@ -0,0 +1,43 @@
+//
+//  main.m
+//  Lab8
+//
+//  Created by Carlos andres Diaz bravo  on 2020-04-06.
+//  Copyright © 2020 Carlos andres Diaz bravo . All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "Kitchen.h"
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSLog(@"Please pick your pizza size and toppings:");
+        
+        Kitchen *restaurantKitchen = [Kitchen new];
+        
+        while (TRUE) {
+            // Loop forever
+            
+            NSLog(@"> ");
+            char str[100];
+            fgets (str, 100, stdin);
+            
+            NSString *inputString = [[NSString alloc] initWithUTF8String:str];
+            inputString = [inputString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+            
+            NSLog(@"Input was %@", inputString);
+            
+            // Take the first word of the command as the size, and the rest as the toppings
+            NSArray *commandWords = [inputString componentsSeparatedByString:@" "];
+            
+            // And then send some message to the kitchen...
+        }
+
+    }
+    return 0;
+}
+
